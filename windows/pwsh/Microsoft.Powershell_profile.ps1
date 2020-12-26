e791874c7d8543d43fc4cfd882f03013526f5f78
Bram Ceulemans <bramceulemans@me.com>
2020-12-26 17:47:41 +0100

    Add Merge-Cuesheet function to PowerShell profile

--- windows/pwsh/Microsoft.Powershell_profile.ps1
+++ windows/pwsh/Microsoft.Powershell_profile.ps1
@@ -6,6 +6,14 @@ function CdRij () {
     Set-Location -Path "C:\src\rijnijssel\"
 }
 
+function Merge-Cuesheet {
+    param (
+        $Cue,
+        $Flac
+    )
+    metaflac --set-tag-from-file="CUESHEET=${Cue}" --import-cuesheet-from="${Cue}" "${Flac}"
+}
+
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme AgnosterPlus
